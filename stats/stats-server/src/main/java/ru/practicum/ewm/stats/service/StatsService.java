5795c27b3aa49b902d6a646ca309872da20d71e1
chuvi89 <chuvi.men@gmail.com>
2025-08-22 22:14:04 +0300

    test

--- stats/stats-server/src/main/java/ru/practicum/ewm/stats/service/StatsService.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/stats/service/StatsService.java
@@ -9,5 +9,6 @@ import java.util.List;
 
 public interface StatsService {
     EndpointHitResponse createHit(EndpointHitRequest request);
+
     List<ViewStatsResponse> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique);
 }
