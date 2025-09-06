cfd220dcf4e8a4a0b633eebfdf7b306462d98bd4
chuvi89 <chuvi.men@gmail.com>
2025-09-07 01:26:35 +0300

    first edit

--- ewm-service/src/main/java/ru/practicum/ewm/dto/CommentDto.java
+++ ewm-service/src/main/java/ru/practicum/ewm/dto/CommentDto.java
@@ -1,12 +1,10 @@
 package ru.practicum.ewm.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
